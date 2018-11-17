0db02f6c52b17888d1680c7d4ea23589e8e23ea7
mrsekut <k.cloudspider@gmail.com>
2018-11-17 22:53:26 +0900

    [Add]merge test

--- tests/test1.nim
+++ tests/test1.nim
@@ -3,60 +3,6 @@ import lexer/lexer
 import lexer/token
 
 suite "Lexer":
-  test "it analysis simple token":
-    let input: string = """
-      =+(){},:
-      !-/*5
-      5 < 10 > 5
-
-      if 5 > 10:
-        return true
-      else:
-        return false
-    """
-
-    let test = @[
-        ( token.ASSiGN, "=" ),
-        ( token.PLUS, "+" ),
-        ( token.LPAREN, "(" ),
-        ( token.RPAREN, ")" ),
-        ( token.LBRACE, "{" ),
-        ( token.RBRACE, "}"),
-        ( token.COMMA, "," ),
-        ( token.COLON, ":" ),
-        ( token.BANG, "!" ),
-        ( token.MINUS, "-" ),
-        ( token.SLASH, "/" ),
-        ( token.ASTERISC, "*" ),
-        ( token.INT, "5" ),
-        ( token.INT, "5" ),
-        ( token.LT, "<" ),
-        ( token.INT, "10" ),
-        ( token.GT, ">" ),
-        ( token.INT, "5" ),
-
-        ( token.IF, "if" ),
-        ( token.INT, "5" ),
-        ( token.GT, ">" ),
-        ( token.INT, "10" ),
-        ( token.COLON, ":" ),
-        ( token.RETURN, "return" ),
-        ( token.TRUE, "true" ),
-        ( token.ELSE, "else" ),
-        ( token.COLON, ":" ),
-        ( token.RETURN, "return" ),
-        ( token.FALSE, "false" ),
-        # # ( token.EOF, "" ),
-    ]
-
-    var l = lexer.newLexer(input)
-
-    for i in test:
-        let tok = l.nextToken()
-        check(i[0] == tok.Type)
-        check(i[1] == tok.Literal)
-
-  # NOTE: あとでテストマージしよう
   test "it analysis simple code":
     let input: string = """
       let five = 5
@@ -66,6 +12,14 @@ suite "Lexer":
         x + y
 
       let result = add(five, ten)
+
+      !-/*5
+      5 < 10 > 5
+
+      if 5 > 10:
+        return true
+      else:
+        return false
     """
 
     let test = @[
@@ -97,6 +51,28 @@ suite "Lexer":
         ( token.COMMA, "," ),
         ( token.IDENT, "ten" ),
         ( token.RPAREN, ")" ),
+
+        ( token.BANG, "!" ),
+        ( token.MINUS, "-" ),
+        ( token.SLASH, "/" ),
+        ( token.ASTERISC, "*" ),
+        ( token.INT, "5" ),
+        ( token.INT, "5" ),
+        ( token.LT, "<" ),
+        ( token.INT, "10" ),
+        ( token.GT, ">" ),
+        ( token.INT, "5" ),
+        ( token.IF, "if" ),
+        ( token.INT, "5" ),
+        ( token.GT, ">" ),
+        ( token.INT, "10" ),
+        ( token.COLON, ":" ),
+        ( token.RETURN, "return" ),
+        ( token.TRUE, "true" ),
+        ( token.ELSE, "else" ),
+        ( token.COLON, ":" ),
+        ( token.RETURN, "return" ),
+        ( token.FALSE, "false" ),
         # ( token.EOF, "" ),
     ]
 
