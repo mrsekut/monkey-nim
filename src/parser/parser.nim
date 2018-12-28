055525be1d15f88396220affb76d68f2bd501d41
mrsekut <k.cloudspider@gmail.com>
2018-12-28 11:45:50 +0900

    [Fix]method

--- src/parser/parser.nim
+++ src/parser/parser.nim
@@ -2,7 +2,6 @@ import ast
 import ../lexer/lexer
 import ../lexer/token
 import sequtils, strformat
-# NOTE: やっぱ`method`にしようぜ
 
 
 type Parser* = ref object of RootObj
@@ -11,18 +10,18 @@ type Parser* = ref object of RootObj
     peekToken: Token
     errors: seq[string]
 
-proc nextToken(self: Parser) =
+method nextToken(self: Parser) =
         self.curToken = self.peekToken
         self.peekToken = self.l.nextToken()
 
-proc curTokenIs(self: Parser, t: TokenType): bool = self.curToken.Type == t
-proc peekTokenIs(self: Parser, t: TokenType): bool = self.peekToken.Type == t
+method curTokenIs(self: Parser, t: TokenType): bool = self.curToken.Type == t
+method peekTokenIs(self: Parser, t: TokenType): bool = self.peekToken.Type == t
 
-proc peekError(self: Parser, t: token.TokenType) =
+method peekError(self: Parser, t: token.TokenType) =
     let msg = fmt"expected next tokent to be {t}, got {self.peekToken.Type} instead"
     self.errors.add(msg)
 
-proc expectPeek(self: Parser, t: token.TokenType): bool =
+method expectPeek(self: Parser, t: token.TokenType): bool =
     if self.peekTokenIs(t):
         self.nextToken()
         return true
@@ -31,7 +30,7 @@ proc expectPeek(self: Parser, t: token.TokenType): bool =
         return false
 
 
-proc parseLetStatement(self: Parser): Statement =
+method parseLetStatement(self: Parser): Statement =
     var statement = Statement(kind: LetStatement, Token: self.curToken)
 
     # ident
@@ -50,7 +49,7 @@ proc parseLetStatement(self: Parser): Statement =
 
     statement
 
-proc parseReturnStatement(self: Parser): Statement =
+method parseReturnStatement(self: Parser): Statement =
     var statement = Statement(kind: ReturnStatement, Token: self.curToken)
     self.nextToken()
 
@@ -61,7 +60,7 @@ proc parseReturnStatement(self: Parser): Statement =
     statement
 
 
-proc parseStatement(self: Parser): Statement =
+method parseStatement(self: Parser): Statement =
     case self.curToken.Type
     of token.LET:
         return self.parseLetStatement()
@@ -71,7 +70,7 @@ proc parseStatement(self: Parser): Statement =
     #     return cast[None](0)
 
 # create AST Root Node
-proc parseProgram*(self: Parser): Program =
+method parseProgram*(self: Parser): Program =
     var program = Program()
     program.statements = newSeq[Statement]()
 
@@ -83,9 +82,10 @@ proc parseProgram*(self: Parser): Program =
 
     program
 
-proc error*(self: Parser): seq[string] = self.errors
 
-proc newParser*(l: Lexer): Parser =
+method error*(self: Parser): seq[string] = self.errors
+
+method newParser*(l: Lexer): Parser =
     let p = Parser(l: l, errors: newSeq[string]())
     p.nextToken()
     p.nextToken()
