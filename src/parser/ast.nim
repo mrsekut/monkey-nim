4c1bccbdd82e73c4821bb46b77bd6dd1ae83ae3e
mrsekut <k.cloudspider@gmail.com>
2019-01-01 16:34:50 +0900

    [Clean]

--- src/parser/ast.nim
+++ src/parser/ast.nim
@@ -1,20 +1,6 @@
 import strformat, typetraits
 import ../lexer/token
 
-# type Identifier*[T] = ref object of RootObj
-#     Token*: token.Token
-#     Value*: T
-
-# proc expressionNode(self: Identifier) = discard
-# proc tokenLiteral(self: Identifier): string = self.Token.Literal
-# proc astToString(self: Identifier): string =
-#     if self.Value.type.name == "string":
-#         return self.Value
-#     elif self.Value.type.name == "int":
-#         return self.Token.Literal
-
-# NOTE: 一つのオブジェクトしにしたもの =========
-# Error: 同じ名前のプロパティを作れない(Value)
 type
     TIdentifier* = enum Ident, IntegerLiteral
 
@@ -34,28 +20,6 @@ proc astToString(self: Identifier): string =
         return self.IdentValue
     of IntegerLiteral:
         return self.Token.Literal
-# ここまで ============================
-
-
-# NOTE: 本と同じように一つずつ作ったもの。
-# Error: case..ofを使うメソッドの返り値の型を指定できない
-# type Identifier* = ref object of RootObj
-#     Token*: token.Token
-#     Value*: string
-
-# proc expressionNode(self: Identifier) = discard
-# proc tokenLiteral(self: Identifier): string = self.Token.Literal
-# proc astToString(self: Identifier): string = self.Value
-
-
-# type IntegerLiteral* = ref object of RootObj
-#     Token*: token.Token
-#     Value*: int64
-
-# proc expressionNode(self: IntegerLiteral) = discard
-# proc tokenLiteral(self: IntegerLiteral): string = self.Token.Literal
-# proc astToString(self: IntegerLiteral): string = self.Token.Literal
-# ここまで ============================
 
 
 # Statement
