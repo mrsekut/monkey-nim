ab7c2b0be486dea117c158c4e399819a55389247
mrsekut <k.cloudspider@gmail.com>
2019-01-12 20:09:40 +0900

    [Fix]p.55 test success

--- src/parser/ast.nim
+++ src/parser/ast.nim
@@ -32,12 +32,12 @@ type
         of nkIntegerLiteral:
             IntValue*: int
         of nkLetStatement:
-            Name*: PNode # Identifier
-            Value*:  PNode # Identifier
+            Name*: PNode
+            Value*:  PNode
         of nkReturnStatement:
-            ReturnValue*: PNode # Identifier
+            ReturnValue*: PNode
         of nkExpressionStatement:
-            Expression*: PNode # Identifier
+            Expression*: PNode
         of nkPrefixExpression:
             Right: TNodeSeq
         of Nil:
@@ -47,7 +47,7 @@ type
 
 proc expressionNode(self: var PNode)
 proc tokenLiteral(self: PNode): string
-# proc astToString(self: PNode): string
+proc astToString(self: PNode): string
 
 
 # implementation
@@ -56,150 +56,49 @@ proc expressionNode(self: var PNode) = discard
 
 proc tokenLiteral(self: PNode): string = self.Token.Literal
 
-# proc astToString(self: PNode): string =
-#     case self.kind:
-#     of Ident: result =  self.IdentValue
+proc astToString(self: PNode): string =
+    case self.kind:
+    # of Ident: result =  self.IdentValue
 #     of nkIntegerLiteral: result =  self.Token.Literal
-#     of nkLetStatement:
-#         # let name = <expression>;
-#         # result = fmt"{self.tokenLiteral()} {$self.Name} = {$self.Value};"
-#         result = "nkLetStatement dayo"
-#         # result = fmt"{self.tokenLiteral()} {self.Name.astToString()} = {self.Value.astToString()};"
+    of nkLetStatement:
+        # let name = <expression>;
+        result = fmt"{self.tokenLiteral()} {self.Name.Token.Literal} = {self.Value.Token.Literal};"
 
-#         # NOTE: nilあり版
-#         # o = fmt"{self.tokenLiteral()} {self.Name.astToString()} = "
-#         # if self.Value != nil:
-#         #     o = o & self.Value.astToString()
-#         # result = o & ";"
+        # NOTE: nilあり版
+        # o = fmt"{self.tokenLiteral()} {self.Name.astToString()} = "
+        # if self.Value != nil:
+        #     o = o & self.Value.astToString()
+        # result = o & ";"
 
-#     of nkReturnStatement:
+    of nkReturnStatement:
 #         # return hoge;
 #         # NOTE: 仮 p.53
-#         result = fmt"{self.tokenLiteral()}  returnValueString;"
+        result = fmt"{self.tokenLiteral()}  returnValueString;"
 
 #         # self.ReturnValue.astToString()
 
-#     of nkExpressionStatement:
+    of nkExpressionStatement:
 #         # NOTE: 仮 p.53
-#         result = "ExpressionStatementString"
+        result = "ExpressionStatementString dayo"
 
 #     # of nkPrefixExpression:
 #     #     result = fmt"({self.Token.Literal}{self.Right.astToString()})"
 #         # result = fmt"({self.Token.Literal}{self.Right.astToString()})"
 
-#     else: discard
+    else:
+        result = "仮astToString"
 
 # Program
 type
     Program* = object of RootObj
         statements*: seq[PNode]
 
-# proc astToString*(self: Program): string =
-#     result = ""
-#     for statement in self.statements:
-#         result = result & statement.astToString()
+proc astToString*(self: Program): string =
+    result = ""
+    for statement in self.statements:
+        result = result & statement.astToString()
 
 
-
-# ============================
-# ============================
-# ============================
-# NOTE: 修正
-# ============================
-# ============================
-# ============================
-
-# type
-#     TIdentifier* = enum Ident, IntegerLiteral, IdentNil
-
-#     Identifier* = object of RootObj
-#         Token*: token.Token
-#         case kind*: TIdentifier
-#         of Ident:
-#             IdentValue*: string
-#         of IntegerLiteral:
-#             IntValue*: int
-#         of IdentNil:
-#             discard
-
-# proc expressionNode(self: Identifier) = discard
-# proc tokenLiteral(self: Identifier): string = self.Token.Literal
-# proc astToString(self: Identifier): string =
-#     case self.kind:
-#     of Ident:
-#         return self.IdentValue
-#     of IntegerLiteral:
-#         return self.Token.Literal
-#     else: discard
-
-# type PrefixExpression* = object of RootObj
-#     Token*: token.Token
-#     Right*: Identifier
-
-# proc expressionNode(self: PrefixExpression) = discard
-# proc tokenLiteral(self: PrefixExpression): string = self.Token.Literal
-# proc astToString(self: PrefixExpression): string =
-#     fmt"({self.Token.Literal}{self.Right.astToString()})"
-
-
-
-# Statement
-# type
-#     TStatement* = enum LetStatement, ReturnStatement, ExpressionStatement, Nil
-
-#     Statement* = object of RootObj
-#         Token*: token.Token
-
-#         case kind*: TStatement
-#         of LetStatement:
-#             Name*: Identifier
-#             Value*: Identifier
-#         of ReturnStatement:
-#             ReturnValue*: Identifier
-#         of ExpressionStatement:
-#             Expression*: Identifier
-#         of Nil:
-#             nil
-
-# proc tokenLiteral*(self: Statement): string =
-#     self.Token.Literal
-
-# proc astToString(self: Statement): string =
-#     case self.kind
-#     of LetStatement:
-#         # let name = <expression>;
-#         result = fmt"{self.tokenLiteral()} {self.Name.astToString()} = {self.Value.astToString()};"
-
-#         # NOTE: nilあり版
-#         # o = fmt"{self.tokenLiteral()} {self.Name.astToString()} = "
-#         # if self.Value != nil:
-#         #     o = o & self.Value.astToString()
-#         # result = o & ";"
-
-#     of ReturnStatement:
-#         # return hoge;
-#         # NOTE: 仮 p.53
-#         result = fmt"{self.tokenLiteral()}  returnValueString;"
-
-#         # self.ReturnValue.astToString()
-
-#     of ExpressionStatement:
-#         # NOTE: 仮 p.53
-#         result = "ExpressionStatementString"
-
-#     of Nil:
-#         discard
-
-
-
-# # Program
-# type Program* = object of RootObj
-#     statements*: seq[Statement]
-
-# proc astToString*(self: Program): string =
-#     var o = ""
-#     for statement in self.statements:
-#         o = o & statement.astToString()
-
-#     return o
-
+proc main() = discard
+when isMainModule:
+    main()
--- tests/test_ast.nim
+++ tests/test_ast.nim
@@ -1,31 +1,31 @@
 import
-    unittest, strformat
-    # ../src/parser/ast,
-    # ../src/parser/parser,
-    # ../src/lexer/lexer,
-    # ../src/lexer/token
+    unittest, strformat,
+    ../src/parser/ast,
+    ../src/parser/parser,
+    ../src/lexer/lexer,
+    ../src/lexer/token
 
 suite "AST":
     test "it should parse index letStatement":
         check(true==true)
-#         let program = Program(
-#             statements: @[
-#                 Statement(
-#                     kind: LetStatement,
-#                     Token: Token(Type: token.LET, Literal: "let"),
-#                     Name: Identifier(
-#                         kind: TIdentifier.Ident,
-#                         Token: Token(Type: token.IDENT, Literal: "myVar"),
-#                         IdentValue: "myVar"
-#                     ),
-#                     Value: Identifier(
-#                         kind: TIdentifier.Ident,
-#                         Token: Token(Type: token.IDENT, Literal: "anotherVar"),
-#                         IdentValue: "anotherVar"
-#                     )
-#                 )
-#             ]
-#         )
+        let program = Program(
+            statements: @[
+                PNode(
+                    kind: nkLetStatement,
+                    Token: Token(Type: token.LET, Literal: "let"),
+                    Name: PNode(
+                        kind: nkIdent,
+                        Token: Token(Type: token.IDENT, Literal: "myVar"),
+                        IdentValue: "myVar"
+                    ),
+                    Value: PNode(
+                        kind: nkIdent,
+                        Token: Token(Type: token.IDENT, Literal: "anotherVar"),
+                        IdentValue: "anotherVar"
+                    )
+                )
+            ]
+        )
 
-#         let input: string = "let myVar = anotherVar;"
-#         check(program.astToString() == input)
+        let input = "let myVar = anotherVar;"
+        check(program.astToString() == input)
