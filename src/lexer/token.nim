040f23fa3e647eace5c10518805c06170f50aedc
mrsekut <k.cloudspider@gmail.com>
2019-01-12 22:56:28 +0900

    [Clean]

--- src/lexer/token.nim
+++ src/lexer/token.nim
@@ -10,41 +10,41 @@ type
 
 # TODO: enum
 const
-    ILLEGAL*  = "ILLEGAL"
-    EOF*      = "EOF"
+    ILLEGAL*   = "ILLEGAL"
+    EOF*       = "EOF"
 
-    IDENT*    = "IDENT"
-    INT*      = "INT"
+    IDENT*     = "IDENT"
+    INT*       = "INT"
 
-    ASSIGN*   = "="
-    PLUS*     = "+"
-    MINUS*    = "-"
-    BANG*     = "!"
-    ASTERISC* = "*"
-    SLASH*    = "/"
+    ASSIGN*    = "="
+    PLUS*      = "+"
+    MINUS*     = "-"
+    BANG*      = "!"
+    ASTERISC*  = "*"
+    SLASH*     = "/"
 
-    LT*       = "<"
-    GT*       =" >"
+    LT*        = "<"
+    GT*        =" >"
 
-    COMMA*    = ","
-    COLON*    = ":"
-    SEMICOLON*    = ";"
+    COMMA*     = ","
+    COLON*     = ":"
+    SEMICOLON* = ";"
 
-    LPAREN*   = "("
-    RPAREN*   = ")"
-    LBRACE*   = "{"
-    RBRACE*   = "}"
+    LPAREN*    = "("
+    RPAREN*    = ")"
+    LBRACE*    = "{"
+    RBRACE*    = "}"
 
-    PROC*     = "PROC"
-    LET*      = "LET"
-    TRUE*     = "TRUE"
-    FALSE*    = "FALSE"
-    IF*       = "IF"
-    ELSE*     = "ELSE"
-    RETURN*   = "RETURN"
+    PROC*      = "PROC"
+    LET*       = "LET"
+    TRUE*      = "TRUE"
+    FALSE*     = "FALSE"
+    IF*        = "IF"
+    ELSE*      = "ELSE"
+    RETURN*    = "RETURN"
 
-    EQ*       = "=="
-    NOT_EQ*   = "!="
+    EQ*        = "=="
+    NOT_EQ*    = "!="
 
 
 var keywords = {
